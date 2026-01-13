9bc346496f983d392dc83805d1bd2d959359de4a
SHANTANU-RAI <raishantanu05@gmail.com>
2026-01-14 02:16:20 +0530

    feat: add centralized error handling

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const commuteRoutes = require("./routes/commute");
+const { errorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
 app.use(express.json());
 
 app.use("/commute", commuteRoutes);
 
-
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
